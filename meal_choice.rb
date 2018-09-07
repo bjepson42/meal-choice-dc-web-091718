0c1be37b65f9e90d723c90f70189d50a3b0b21b3
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-09-07 17:05:30 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
-def mealsOnWheels(meal_choice)
+def mealsOnWheels(meal_choice = meat)
   puts 'I want to eat #{meal_choice}.'
 end
